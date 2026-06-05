3ac51b80d55b6a0dcf616b261a2b8295b7d1eecc
izza <muhammadibnzzi@gmail.com>
2026-06-06 00:18:14 +0700

    jalankan-migrasi

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
+
+Route::get('/jalankan-migrasi', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    return "Migrasi berhasil dijalankan!";
+});
+
 
 Route::get('/', function () {
     return view('welcome');
